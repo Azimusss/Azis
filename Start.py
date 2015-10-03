cc6c7386503d643af14b60fb47b29b7fafd84786
azis <myxamad2001@gmail.com>
2015-10-03 16:44:40 +0600

    fffffuuu

--- Start.py
+++ Start.py
@@ -1,4 +1,10 @@
+import os
 import json
+
+def save(data, file_name):
+    file = open(os.path.join(DIR, file_name), 'w', encoding="UTF-8")
+    file.write(json.dumps(data, ensure_ascii=False))
+    file.close()
 
 __author__ = 'azis'
 
@@ -49,7 +55,24 @@ print(namesakes)
 
 print("*"*20,0,"*"*20)
 
+print("*"*18 , "Блок 3" , "*"*17)
 
+print("*"*20,0,"*"*20)
 
-# dsijbccbcdkjkkcbkjc
+DIR = "data"
 
+students_data = json.load(open(os.path.join(DIR, 'Students.json'), 'r'))  # os.path.join - Вставляет нужный \ (Для исскуственного поддержания кросс-платформенности)
+teachers_data = json.load(open(os.path.join(DIR, 'Teachers.json'), 'r'))  # константа - переменная которая не должна изменятся
+
+student = {
+    "name": "Петр",
+    "middle_name": "Алексеевич",
+    "surname": "Первый",
+    "school": "67 школа",
+    "class": "7 В",
+    "birth_day": "06.06.1997"
+}
+
+students_data.append(student)
+
+save(students_data, 'Students.json')
